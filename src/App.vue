8914aa7646bd4b8786c07e958c77930940243594
TonyBrb <anthonybarbagallo95@gmail.com>
2021-11-28 14:32:11 +0100

    Creazione header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="main-wrapper">
+      <Header />
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
+    
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import "./assets/style/general.scss";
+
+.main-wrapper{
+  min-height: 100vh;
+  background-color: #1E2D3B;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img src="../../public/spotify-logo.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lanf="scss">
+  div{
+    height: 50px;
+    background-color: #2E3A46;
+      
+  }
+  img{
+      margin-left: 20px;
+      width: 50px;
+      height: 100%;
+    }
+
+</style>
